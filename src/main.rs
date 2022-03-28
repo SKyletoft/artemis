5635f106d9476b77a3e162c6d41b29f9e40d950e
Samuel Kyletoft <samuel@kyletoft.se>
2022-03-28 17:12:40 +0200

    Add dependencies and examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use bnf::Grammar;
+use smallvec::{SmallVec, smallvec};
+use smallstr::SmallString;
+
 fn main() {
-	println!("Hello, world!");
+    let input = include_str!("artemis.bnf");
+    
+    let grammar: Result<Grammar, _> = input.parse();
+    match grammar {
+        Ok(g) => println!("{:#?}", g),
+        Err(e) => println!("Failed to make grammar from String: {}", e),
+    }
 }
