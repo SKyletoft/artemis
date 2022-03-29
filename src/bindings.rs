22e9d9b64551a4174cf1fe754ee854c312ce109c
Samuel Kyletoft <samuel@kyletoft.se>
2022-03-29 21:41:43 +0200

    Rust wrapper for AST

--- src/bindings.rs
+++ src/bindings.rs
@@ -2,5 +2,74 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
+use std::{convert::AsRef, ffi::CStr, fs, io, os::unix::prelude::AsRawFd, path::Path, fmt::{Debug, Display}};
+
+use anyhow::Result;
+use smallstr::SmallString;
+use parking_lot::Mutex;
+use once_cell::sync::Lazy;
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+static C_PRINTING_LOCK: Lazy<Mutex<()>> = Lazy::new(|| Mutex::new(()));
+
+#[derive(PartialEq, Eq)]
+pub struct AST(Exp);
+
+impl AST {
+	pub fn new(p: impl AsRef<Path>) -> Result<Self> {
+		let file = fs::File::open(p)?;
+		let fd = file.as_raw_fd();
+		// Safety: file descriptor is held until the end of the scope,
+		// resulting AST does not rely on it remaining alive.
+		// Fd <-> Raw pointer is just a C typing difference
+		let c_ast = unsafe { pExp(fd as *mut _) };
+		Ok(Self(c_ast))
+	}
+
+	pub fn from_stdin() -> Self {
+		let rs_stdin = io::stdin();
+		let lock = rs_stdin.lock();
+		let fd = lock.as_raw_fd();
+		// Safety: file descriptor is held until the end of the scope,
+		// resulting AST does not rely on it remaining alive.
+		// Fd <-> Raw pointer is just a C typing difference
+		let c_ast = unsafe { pExp(fd as *mut _) };
+		Self(c_ast)
+	}
+}
+
+impl Drop for AST {
+	fn drop(&mut self) {
+		// Safety: Just a wrapper around the C destructor.
+		// No raw pointers are ever handed out in Rust-land
+		unsafe {
+			free_Exp(self.0);
+		}
+	}
+}
+
+impl Display for AST {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let _lock = C_PRINTING_LOCK.lock();
+	// Safety: C_PRINTING_LOCK accquired, pointer is RAII guarded
+	unsafe {
+		let cstr = CStr::from_ptr(showExp(self.0));
+		let string = cstr.to_string_lossy();
+		write!(f, "{string}")
+	}
+    }
+}
+
+impl Debug for AST {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let _lock = C_PRINTING_LOCK.lock();
+	// Safety: C_PRINTING_LOCK accquired, pointer is RAII guarded
+	unsafe {
+		let cstr = CStr::from_ptr(printExp(self.0));
+		let string = cstr.to_string_lossy();
+		write!(f, "{string}")
+	}
+    }
+}
 
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
-use std::{io, os::unix::io::AsRawFd, ffi::CStr};
-
-
-use smallstr::SmallString;
-use smallvec::{smallvec, SmallVec};
-
 pub mod bindings;
 
+use bindings::AST;
+
 fn main() {
-	unsafe {
-		let stdin = io::stdin();
-		let lock = stdin.lock();
-		let c_compatible_stdin = lock.as_raw_fd();
-
-		let parse_tree = bindings::pExp(c_compatible_stdin as *mut _);
-
-		let s = CStr::from_ptr(bindings::showExp(parse_tree)).to_string_lossy();
-		println!("{s}");
-
-		let s = CStr::from_ptr(bindings::printExp(parse_tree)).to_string_lossy();
-		println!("{s}");
-
-		bindings::free_Exp(parse_tree);
-	}
+	let parse_tree = AST::from_stdin();
+	println!("{0}\n{0:?}", &parse_tree);
 }
