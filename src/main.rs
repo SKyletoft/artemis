91759cfa6e60276d5f7f58883c163aa3d0524dae
Samuel Kyletoft <samuel@kyletoft.se>
2022-03-29 02:47:52 +0200

    Reimplemented the test on the rust side

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+use std::{io, os::unix::io::AsRawFd, ffi::CStr};
+
+
 use smallstr::SmallString;
 use smallvec::{smallvec, SmallVec};
 
 pub mod bindings;
 
 fn main() {
-	println!("Hello, world");
+	unsafe {
+		let stdin = io::stdin();
+		let lock = stdin.lock();
+		let c_compatible_stdin = lock.as_raw_fd();
+
+		let parse_tree = bindings::pExp(c_compatible_stdin as *mut _);
+
+		let s = CStr::from_ptr(bindings::showExp(parse_tree)).to_string_lossy();
+		println!("{s}");
+
+		let s = CStr::from_ptr(bindings::printExp(parse_tree)).to_string_lossy();
+		println!("{s}");
+
+		bindings::free_Exp(parse_tree);
+	}
 }
