584cd083e97e3c60ae934dc1a5fa8d646c8c2db5
Samuel Kyletoft <samuel@kyletoft.se>
2022-04-09 16:23:20 +0200

    Sort test files before running

--- tests/parse.rs
+++ tests/parse.rs
@@ -5,14 +5,17 @@ use pest::Parser;
 
 #[test]
 fn success_files() {
-	let files = fs::read_dir("tests/parse/ok").unwrap();
+	let mut files = fs::read_dir("tests/parse/ok")
+		.unwrap()
+		.map(|entry| entry.unwrap().path())
+		.collect::<Vec<_>>();
+	files.sort_unstable();
 	for file in files {
-		let file = file.unwrap();
-		let contents = fs::read_to_string(file.path()).unwrap();
+		let contents = fs::read_to_string(&file).unwrap();
 		let res = GeneratedParser::parse(Rule::function_definition, &contents);
 
 		if let Err(e) = &res {
-			eprintln!("{:?}:\n{e}", file.path());
+			eprintln!("{:?}:\n{e}", &file);
 		}
 		assert!(res.is_ok());
 	}
@@ -20,14 +23,17 @@ fn success_files() {
 
 #[test]
 fn fail_files() {
-	let files = fs::read_dir("tests/parse/fail").unwrap();
+	let mut files = fs::read_dir("tests/parse/fail")
+		.unwrap()
+		.map(|entry| entry.unwrap().path())
+		.collect::<Vec<_>>();
+	files.sort_unstable();
 	for file in files {
-		let file = file.unwrap();
-		let contents = fs::read_to_string(file.path()).unwrap();
+		let contents = fs::read_to_string(&file).unwrap();
 		let res = GeneratedParser::parse(Rule::function_definition, &contents);
 
 		if let Ok(e) = &res {
-			eprintln!("{:?}:\n{e}", file.path());
+			eprintln!("{:?}:\n{e}", &file);
 		}
 		assert!(res.is_err());
 	}
