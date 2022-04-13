d01ab59e7dd16735368246f5136b029183e1f662
Samuel Kyletoft <samuel@kyletoft.se>
2022-04-13 23:03:48 +0200

    Restructure main

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 use std::{env, fs};
 
 use anyhow::Result;
-use pest::{iterators::Pair, Parser};
+use pest::Parser;
 use simple_logger::SimpleLogger;
+use smallvec::SmallVec;
 
-use artemis::{GeneratedParser, Rule, PREC_CLIMBER};
+use artemis::{
+	ordered::AST,
+	GeneratedParser, Rule,
+};
 
 fn main() -> Result<()> {
 	SimpleLogger::new().init().expect("Logging init failure");
 	log::warn!("Logging initialised");
 
-	let source_file_name = env::args().skip(1).next().expect("No source file provided");
+	let source_file_name = env::args().nth(1).expect("No source file provided");
 	let source = fs::read_to_string(&source_file_name)?;
 	let ast = GeneratedParser::parse(Rule::function_definition, &source)?;
 	dbg!(&ast);
-	let ordered = PREC_CLIMBER.climb(ast, |pair: Pair<Rule>| {}, |lhs, pair: Pair<Rule>, rhs| ());
+	let ordered = ast
+		.map(AST::try_from)
+		.collect::<Result<SmallVec<[AST; 8]>>>()?;
+	dbg!(&ordered);
 
 	println!("\n---------------------------------------------------\n\n{source}");
 
