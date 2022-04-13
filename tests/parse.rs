e70de0123bbaabf2fd3dfdaff173272ad5d5e124
Samuel Kyletoft <samuel@kyletoft.se>
2022-04-14 01:15:16 +0200

    Test the examples too

--- tests/parse.rs
+++ tests/parse.rs
@@ -4,6 +4,24 @@ use artemis::{GeneratedParser, Rule};
 use pest::Parser;
 
 #[test]
+fn examples() {
+	let mut files = fs::read_dir("examples")
+		.unwrap()
+		.map(|entry| entry.unwrap().path())
+		.collect::<Vec<_>>();
+	files.sort_unstable();
+	for file in files {
+		let contents = fs::read_to_string(&file).unwrap();
+		let res = GeneratedParser::parse(Rule::function_definition, &contents);
+
+		if let Err(e) = &res {
+			eprintln!("{:?}:\n{e}", &file);
+		}
+		assert!(res.is_ok());
+	}
+}
+
+#[test]
 fn success_files() {
 	let mut files = fs::read_dir("tests/parse/ok")
 		.unwrap()
