4080b5a445e6b96a29507afb9dd4c9113701ba17
Samuel Kyletoft <samuel@kyletoft.se>
2022-04-13 23:03:00 +0200

    MaybeParsed

--- src/ordered.rs
+++ src/ordered.rs
@@ -158,3 +158,105 @@ macro_rules! remove_by_pattern {
 	};
 }
 
+#[derive(Debug, Clone, PartialEq, Variantly)]
+enum MaybeParsed<'a> {
+	Parsed(AST),
+	Unparsed(Pair<'a, Rule>),
+	Operator(Op),
+	Empty,
+}
+
+impl<'a> TryFrom<Pair<'a, Rule>> for MaybeParsed<'a> {
+	type Error = anyhow::Error;
+
+	fn try_from(pair: Pair<'a, Rule>) -> Result<MaybeParsed<'a>, Self::Error> {
+		match pair.as_rule() {
+			Rule::term => {
+				let inner = pair
+					.into_inner()
+					.next()
+					.expect("Rule should always contain exactly one subrule");
+				let subexpr = match inner.as_rule() {
+					Rule::float => Subexpr::Literal(Literal::Float(inner.as_str().parse()?)),
+					Rule::integer => {
+						// Try to parse as unsigned, on fail, retry as signed
+						// instead and bitcast to unsigned because our IR
+						// only supports unsigned
+						let val = inner.as_str().parse::<u64>().or_else(|_| -> Result<u64> {
+							Ok(inner.as_str().parse::<i64>()? as u64)
+						})?;
+						Subexpr::Literal(Literal::Integer(val))
+					}
+					Rule::boolean => Subexpr::Literal(Literal::Boolean(inner.as_str().parse()?)),
+					Rule::unit => Subexpr::Literal(Literal::Unit),
+					Rule::tuple => {
+						let tuple = inner.into_inner()
+							.into_iter()
+							.map(|pair| {
+								let expr = AST::try_from(pair)?
+									.subexpr()
+									.ok_or(Error::ParseError)?;
+								Ok(expr)
+							})
+							.collect::<Result<Vec<_>>>()?;
+						Subexpr::Tuple(tuple)
+					},
+					Rule::subexpr => {
+						// Parentheses
+						AST::try_from(inner)?.subexpr().ok_or(Error::ParseError)?
+					}
+					Rule::block => todo!(),
+					Rule::if_expr => todo!(),
+					Rule::function_call => {
+						let mut inner_2 = inner
+							.into_inner()
+							.map(AST::try_from)
+							.collect::<Result<SmallVec<[AST; 8]>>>()?;
+						// There is always at least the name of the function being called
+						assert!(!inner_2.is_empty());
+						let function_name = inner_2
+							.remove(0)
+							.subexpr()
+							.and_then(Subexpr::variable)
+							.ok_or(Error::Internal)?;
+						let arguments = inner_2
+							.into_iter()
+							.map(AST::subexpr)
+							.collect::<Option<Vec<_>>>()
+							.ok_or(Error::ParseError)?;
+						Subexpr::FunctionCall(FunctionCall {
+							function_name,
+							arguments,
+						})
+					}
+					Rule::var_name => Subexpr::Variable(inner.as_str().into()),
+					_ => bail!(Error::Internal),
+				};
+				Ok(MaybeParsed::Parsed(AST::Subexpr(subexpr)))
+			}
+			Rule::operator => {
+				let inner_op = pair
+					.into_inner()
+					.next()
+					.expect("Rule should always contain exactly one subrule")
+					.as_rule();
+				let op = match inner_op {
+					Rule::plus => Op::Plus,
+					Rule::minus => Op::Minus,
+					Rule::delta => Op::Delta,
+					Rule::times => Op::Times,
+					Rule::div => Op::Div,
+					Rule::exp => Op::Exp,
+					Rule::not => Op::Not,
+					Rule::and => Op::And,
+					Rule::or => Op::Or,
+					Rule::xor => Op::Xor,
+					_ => bail!(Error::Internal),
+				};
+				Ok(MaybeParsed::Operator(op))
+			}
+			_ => unreachable!(),
+		}
+	}
+}
+
