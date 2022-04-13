76209c0410cdcecbf36bafdc469f6848c5072f90
Samuel Kyletoft <samuel@kyletoft.se>
2022-04-13 23:45:51 +0200

    Add tests

--- tests/order.rs
+++ tests/order.rs
@@ -21,3 +21,108 @@ fn add_2() {
 	assert_eq!(res.len(), 1);
 	assert_eq!(res[0], expected);
 }
+
+#[test]
+fn add_3() {
+	let s = "1 + 2 + 3";
+	let res = GeneratedParser::parse(Rule::subexpr, s)
+		.unwrap()
+		.map(AST::try_from)
+		.collect::<Result<Vec<_>>>()
+		.unwrap();
+	let expected = AST::Subexpr(Subexpr::BinOp(BinOp {
+		lhs: Box::new(Subexpr::BinOp(BinOp {
+			lhs: Box::new(Subexpr::Literal(Literal::Integer(1))),
+			op: Op::Plus,
+			rhs: Box::new(Subexpr::Literal(Literal::Integer(2))),
+		})),
+		op: Op::Plus,
+		rhs: Box::new(Subexpr::Literal(Literal::Integer(3))),
+	}));
+	assert_eq!(res.len(), 1);
+	assert_eq!(res[0], expected);
+}
+
+#[test]
+fn mul_front() {
+	let s = "1 * 2 + 3";
+	let res = GeneratedParser::parse(Rule::subexpr, s)
+		.unwrap()
+		.map(AST::try_from)
+		.collect::<Result<Vec<_>>>()
+		.unwrap();
+	let expected = AST::Subexpr(Subexpr::BinOp(BinOp {
+		lhs: Box::new(Subexpr::BinOp(BinOp {
+			lhs: Box::new(Subexpr::Literal(Literal::Integer(1))),
+			op: Op::Times,
+			rhs: Box::new(Subexpr::Literal(Literal::Integer(2))),
+		})),
+		op: Op::Plus,
+		rhs: Box::new(Subexpr::Literal(Literal::Integer(3))),
+	}));
+	assert_eq!(res.len(), 1);
+	assert_eq!(res[0], expected);
+}
+
+#[test]
+fn mul_back() {
+	let s = "1 + 2 * 3";
+	let res = GeneratedParser::parse(Rule::subexpr, s)
+		.unwrap()
+		.map(AST::try_from)
+		.collect::<Result<Vec<_>>>()
+		.unwrap();
+	let expected = AST::Subexpr(Subexpr::BinOp(BinOp {
+		lhs: Box::new(Subexpr::Literal(Literal::Integer(1))),
+		op: Op::Plus,
+		rhs: Box::new(Subexpr::BinOp(BinOp {
+			lhs: Box::new(Subexpr::Literal(Literal::Integer(2))),
+			op: Op::Times,
+			rhs: Box::new(Subexpr::Literal(Literal::Integer(3))),
+		})),
+	}));
+	assert_eq!(res.len(), 1);
+	assert_eq!(res[0], expected);
+}
+
+#[test]
+fn parenthesis_1() {
+	let s = "(1 + 2) * 3";
+	let res = GeneratedParser::parse(Rule::subexpr, s)
+		.unwrap()
+		.map(AST::try_from)
+		.collect::<Result<Vec<_>>>()
+		.unwrap();
+	let expected = AST::Subexpr(Subexpr::BinOp(BinOp {
+		lhs: Box::new(Subexpr::BinOp(BinOp {
+			lhs: Box::new(Subexpr::Literal(Literal::Integer(1))),
+			op: Op::Plus,
+			rhs: Box::new(Subexpr::Literal(Literal::Integer(2))),
+		})),
+		op: Op::Times,
+		rhs: Box::new(Subexpr::Literal(Literal::Integer(3))),
+	}));
+	assert_eq!(res.len(), 1);
+	assert_eq!(res[0], expected);
+}
+
+#[test]
+fn parenthesis_2() {
+	let s = "1 * (2 + 3)";
+	let res = GeneratedParser::parse(Rule::subexpr, s)
+		.unwrap()
+		.map(AST::try_from)
+		.collect::<Result<Vec<_>>>()
+		.unwrap();
+	let expected = AST::Subexpr(Subexpr::BinOp(BinOp {
+		lhs: Box::new(Subexpr::Literal(Literal::Integer(1))),
+		op: Op::Times,
+		rhs: Box::new(Subexpr::BinOp(BinOp {
+			lhs: Box::new(Subexpr::Literal(Literal::Integer(2))),
+			op: Op::Plus,
+			rhs: Box::new(Subexpr::Literal(Literal::Integer(3))),
+		})),
+	}));
+	assert_eq!(res.len(), 1);
+	assert_eq!(res[0], expected);
+}
