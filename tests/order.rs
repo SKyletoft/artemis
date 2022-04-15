bb5ae4168d10c34aaf13d833f314068e207963cb
Samuel Kyletoft <samuel@kyletoft.se>
2022-04-15 14:06:41 +0200

    Tests for parsing function calls

--- tests/order.rs
+++ tests/order.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use artemis::{
-	ordered::{BinOp, Literal, Op, Subexpr, AST},
+	ordered::{BinOp, FunctionCall, Literal, Op, Subexpr, AST},
 	GeneratedParser, Rule,
 };
 use pest::Parser;
@@ -125,4 +125,85 @@ fn parenthesis_2() {
 	}));
 	assert_eq!(res.len(), 1);
 	assert_eq!(res[0], expected);
+}
+
+#[test]
+fn function_call_0() {
+	let s = "f()";
+	let res = GeneratedParser::parse(Rule::subexpr, s)
+		.unwrap()
+		.map(AST::try_from)
+		.collect::<Result<Vec<_>>>()
+		.unwrap();
+	let expected = AST::Subexpr(Subexpr::FunctionCall(FunctionCall {
+		function_name: "f".into(),
+		arguments: vec![],
+	}));
+	assert_eq!(res.len(), 1);
+	assert_eq!(res[0], expected);
+}
+
+#[test]
+fn function_call_1() {
+	let s = "f(x)";
+	let res = GeneratedParser::parse(Rule::subexpr, s)
+		.unwrap()
+		.map(AST::try_from)
+		.collect::<Result<Vec<_>>>()
+		.unwrap();
+	let expected = AST::Subexpr(Subexpr::FunctionCall(FunctionCall {
+		function_name: "f".into(),
+		arguments: vec![Subexpr::Variable("x".into())],
+	}));
+	assert_eq!(res.len(), 1);
+	assert_eq!(res[0], expected);
+}
+
+#[test]
+fn function_call_2() {
+	let s = "f(x, y)";
+	let res = GeneratedParser::parse(Rule::subexpr, s)
+		.unwrap()
+		.map(AST::try_from)
+		.collect::<Result<Vec<_>>>()
+		.unwrap();
+	let expected = AST::Subexpr(Subexpr::FunctionCall(FunctionCall {
+		function_name: "f".into(),
+		arguments: vec![Subexpr::Variable("x".into()), Subexpr::Variable("y".into())],
+	}));
+	assert_eq!(res.len(), 1);
+	assert_eq!(res[0], expected);
+}
+
+#[test]
+fn function_call_3() {
+	let s = "f(x, y, x + y)";
+	let res = GeneratedParser::parse(Rule::subexpr, s)
+		.unwrap()
+		.map(AST::try_from)
+		.collect::<Result<Vec<_>>>()
+		.unwrap();
+	let expected = AST::Subexpr(Subexpr::FunctionCall(FunctionCall {
+		function_name: "f".into(),
+		arguments: vec![
+			Subexpr::Variable("x".into()),
+			Subexpr::Variable("y".into()),
+			Subexpr::BinOp(BinOp {
+				lhs: Box::new(Subexpr::Variable("x".into())),
+				op: Op::Plus,
+				rhs: Box::new(Subexpr::Variable("y".into())),
+			}),
+		],
+	}));
+	dbg!(&res);
+	assert_eq!(res.len(), 1);
+	assert_eq!(res[0], expected);
+}
+
+#[test]
+fn function_call_trailing_comma() {
+	let s = "f(x,)";
+	let res = GeneratedParser::parse(Rule::function_call, s);
+	dbg!(&res);
+	assert!(res.is_err());
 }
