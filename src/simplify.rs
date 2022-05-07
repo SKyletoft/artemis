9966f9966ddf06ae5939ab0fe7cc768dda3db7db
Samuel Kyletoft <samuel@kyletoft.se>
2022-05-08 01:34:28 +0200

    V1 of thoughts on if statements

--- src/simplify.rs
+++ src/simplify.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, mem};
 
 use anyhow::{bail, Result};
 use derive_more::{Add, AddAssign, From};
@@ -6,21 +6,23 @@ use smallvec::SmallVec;
 use variantly::Variantly;
 
 use crate::{
+	detype::{Assignment, BinOp, Declaration, Expr, FunctionCall, IfExpr, Op, Subexpr},
 	error::Error,
-	ordered::{BinOp, FunctionCall, IfExpr, Literal, Op, Subexpr},
 };
 
 type SmallString = smallstr::SmallString<[u8; 16]>;
 
 #[derive(Debug, Clone, Copy, Add, PartialEq, Eq, PartialOrd, Ord, AddAssign, Default, From)]
-pub struct Register(u32);
+#[repr(transparent)]
+pub struct Register(usize);
 
 #[derive(Debug, Clone, Copy, Add, PartialEq, Eq, PartialOrd, Ord, AddAssign, Default, From)]
-pub struct BlockId(u32);
+#[repr(transparent)]
+pub struct BlockId(usize);
 
 #[derive(Debug, Clone, PartialEq, Default)]
 pub struct Context {
-	pub variables: HashMap<SmallString, Register>,
+	pub variables: HashMap<SmallString, Source>,
 	pub next_register: Register,
 }
 
@@ -32,19 +34,31 @@ impl Context {
 	}
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum BlockEnd {
+	Return,
+	One(BlockId),
+	Two(Source, BlockId, BlockId),
+}
+
+impl Default for BlockEnd {
+	fn default() -> Self {
+		BlockEnd::Return
+	}
+}
+
 #[derive(Debug, Clone, PartialEq, Default)]
-struct Block {
-	id: BlockId,
-	intro: (), // Phi nodes
-	block: SmallVec<[SimpleExpression; 4]>,
-	out: () // Jumps
+pub struct Block {
+	pub id: BlockId,
+	pub intro: (), // Phi nodes
+	pub block: SmallVec<[SimpleExpression; 4]>,
+	pub out: BlockEnd,
 }
 
 #[derive(Debug, Copy, Clone, PartialEq, Variantly)]
 pub enum Source {
 	Register(Register),
-	Integer(u64),
-	Float(f64),
+	Value(u64),
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -103,7 +117,8 @@ pub enum SimpleExpression {
 
 pub fn simplify_subexpr(
 	subexpr: &Subexpr,
-	out: &mut Vec<SimpleExpression>,
+	current: &mut Block,
+	blocks: &mut Vec<Option<Block>>,
 	ctx: &mut Context,
 ) -> Result<Source> {
 	let res = match subexpr {
@@ -118,23 +133,34 @@ pub fn simplify_subexpr(
 		}) => todo!("transform into sub + abs"),
 		Subexpr::BinOp(BinOp {
 			lhs,
+			op: Op::FDelta,
+			rhs,
+		}) => todo!("transform into sub + abs"),
+		Subexpr::BinOp(BinOp {
+			lhs,
 			op: Op::Exp,
 			rhs,
 		}) => todo!("function call?"),
 		Subexpr::BinOp(BinOp { lhs, op, rhs }) => {
-			let left = simplify_subexpr(lhs, out, ctx)?;
-			let right = simplify_subexpr(rhs, out, ctx)?;
+			let left = simplify_subexpr(lhs, current, blocks, ctx)?;
+			let right = simplify_subexpr(rhs, current, blocks, ctx)?;
 			let target = ctx.next();
 			let simple_operator = match op {
 				Op::Plus => SimpleOp::Add,
+				Op::FPlus => SimpleOp::FAdd,
 				Op::Minus => SimpleOp::Sub,
+				Op::FMinus => SimpleOp::FSub,
 				Op::Times => SimpleOp::Mul,
+				Op::FTimes => SimpleOp::FMul,
 				Op::Div => SimpleOp::Div,
+				Op::FDiv => SimpleOp::FDiv,
 				Op::And => SimpleOp::And,
 				Op::Or => SimpleOp::Or,
 				Op::Xor => SimpleOp::Xor,
 				Op::Dot => todo!("Structs aren't implemented yet"),
-				Op::Delta | Op::Exp | Op::Not => unreachable!(),
+				Op::Delta | Op::FDelta | Op::Exp | Op::FExp | Op::Not => {
+					unreachable!("Separate cases above")
+				}
 			};
 			let this = SimpleBinOp {
 				target,
@@ -142,31 +168,43 @@ pub fn simplify_subexpr(
 				lhs: left,
 				rhs: right,
 			};
-			out.push(SimpleExpression::BinOp(this));
+			current.block.push(SimpleExpression::BinOp(this));
 			Source::Register(target)
 		}
 		Subexpr::IfExpr(IfExpr {
 			condition,
 			lhs,
 			rhs,
-		}) => todo!(),
+		}) => {
+			let cond = simplify_subexpr(condition, current, blocks, ctx)?;
+			let cond_idx = blocks.len();
+			blocks.push(None); // Reserve a space for the block we came from
+			let completed_block = mem::replace(current, Block::default());
+			let existing_context = mem::replace(&mut ctx.variables, HashMap::new());
+
+			let (then_idx, then_source) = simplify_exprs(lhs, current, blocks, ctx)?;
+			let then_code = mem::replace(current, Block::default());
+			blocks.push(Some(then_code));
+
+			let else_idx = blocks.len();
+			blocks.push(None);
+			let else_source = simplify_exprs(rhs, current, blocks, ctx)?;
+			let else_code = mem::replace(current, Block::default());
+
+			let block = Block {
+				id: cond_idx.into(),
+				intro: todo!(),
+				block: completed_block,
+				out: BlockEnd::Two(todo!().into(), then_source, else_source),
+			};
+			todo!()
+		}
 		Subexpr::Block(_) => todo!(),
-		Subexpr::Literal(v) => match v {
-			Literal::Integer(i) => Source::Integer(*i),
-			Literal::Float(f) => Source::Float(*f),
-			Literal::Boolean(b) => Source::Integer(*b as u64),
-			Literal::Unit => {
-				log::error!("Internal [{}]: Use of unit value", line!());
-				bail!(Error::Internal);
-			}
-		},
-		Subexpr::Variable(v) => match ctx.variables.get(v) {
-			Some(v) => Source::Register(*v),
-			None => {
-				log::error!("Internal [{}]: Use of undeclared variable", line!());
-				bail!(Error::Internal);
-			}
-		},
+		Subexpr::Literal(v) => Source::Value(*v),
+		Subexpr::Variable(v) => *ctx.variables.get(v).ok_or_else(|| {
+			log::error!("Internal [{}]: Use of undeclared variable", line!());
+			Error::Internal
+		})?,
 		Subexpr::Tuple(_) => {
 			todo!("Should tuples even exist at this stage? Should they be a stack thing? These are design questions, not implementation")
 		}
@@ -176,7 +214,7 @@ pub fn simplify_subexpr(
 		}) => {
 			let args = arguments
 				.iter()
-				.map(|s| simplify_subexpr(s, out, ctx))
+				.map(|s| simplify_subexpr(s, current, blocks, ctx))
 				.collect::<Result<SmallVec<_>>>()?;
 			let target = ctx.next();
 			let call = SimpleFunctionCall {
@@ -184,9 +222,36 @@ pub fn simplify_subexpr(
 				function: function_name.clone(),
 				args,
 			};
-			out.push(SimpleExpression::FunctionCall(call));
+			current.block.push(SimpleExpression::FunctionCall(call));
 			Source::Register(target)
 		}
+		Subexpr::Unit => todo!(),
 	};
 	Ok(res)
+}
+
+pub fn simplify_expr(
+	expr: &Expr,
+	current: &mut Block,
+	blocks: &mut Vec<Option<Block>>,
+	ctx: &mut Context,
+) -> Result<Source> {
+	match expr {
+		Expr::Subexpr(s) => simplify_subexpr(s, current, blocks, ctx),
+		Expr::Declaration(Declaration { name, value })
+		| Expr::Assignment(Assignment { name, value }) => {
+			let source = simplify_subexpr(value, current, blocks, ctx)?;
+			ctx.variables.insert(name.clone(), source);
+			Ok(source)
+		}
+	}
+}
+
+pub fn simplify_exprs(
+	exprs: &[Expr],
+	current: &mut Block,
+	blocks: &mut Vec<Option<Block>>,
+	ctx: &mut Context,
+) -> Result<(BlockId, Source)> {
+	todo!()
 }
