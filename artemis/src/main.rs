89384559ef41a7705c8ee8be755ec948d699c701
Samuel Kyletoft <samuel@kyletoft.se>
2022-07-24 01:31:49 +0200

    Debug print assembly

--- artemis/src/main.rs
+++ artemis/src/main.rs
@@ -71,7 +71,7 @@ fn main() -> Result<()> {
 		register_allocation::register_allocate(&ssa, &Configuration::new(7, 0, 4, 0))?;
 
 	let assembler = x86_64::assemble(&allocated[0])?;
-	eprintln!("{}", &assembler);
+	eprintln!("{:?}", &assembler);
 
 	fs::write("a.asm", assembler)?;
 
