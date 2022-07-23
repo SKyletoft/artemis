7609a264ef91ff725d253d56fb85713036262c35
Samuel Kyletoft <samuel@kyletoft.se>
2022-07-23 15:56:51 +0200

    More x86 assembly

--- air/src/x86_64.rs
+++ air/src/x86_64.rs
@@ -1,11 +1,14 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use iced_x86::{
 	code_asm::{AsmRegister64, CodeAssembler},
 	Formatter, NasmFormatter,
 };
 
-use crate::register_allocation::{
-	BinOp, Block, BlockEnd, CodeConstruct, Expression, FunctionCall, Op, Register, UnOp,
+use crate::{
+	error::Error,
+	register_allocation::{
+		BinOp, Block, BlockEnd, CodeConstruct, Expression, FunctionCall, Op, Register, UnOp,
+	},
 };
 
 // SYSTEMV ABI for x64
@@ -58,13 +61,8 @@ fn assemble_block(Block { block, out }: &Block, assembler: &mut CodeAssembler) -
 				Op::Add => assembler.add(GP[l], GP[r])?,
 				Op::Sub => assembler.sub(GP[l], GP[r])?,
 				Op::Abs => todo!(),
-				Op::Mul => todo!(),
+				Op::Mul => assembler.imul_2(GP[l], GP[r])?,
 				Op::Div => todo!(),
-				Op::FAdd => todo!(),
-				Op::FSub => todo!(),
-				Op::FAbs => todo!(),
-				Op::FMul => todo!(),
-				Op::FDiv => todo!(),
 				Op::And => todo!(),
 				Op::Or => todo!(),
 				Op::Xor => todo!(),
@@ -72,6 +70,9 @@ fn assemble_block(Block { block, out }: &Block, assembler: &mut CodeAssembler) -
 				Op::StoreMem => todo!(),
 				Op::LoadMem => todo!(),
 				Op::Move => todo!(),
+				Op::FAdd | Op::FSub | Op::FAbs | Op::FMul | Op::FDiv => {
+					bail!(Error::InvalidIR)
+				}
 			},
 			&Expression::BinOp(BinOp {
 				target: Register::GeneralPurpose(t),
@@ -80,22 +81,55 @@ fn assemble_block(Block { block, out }: &Block, assembler: &mut CodeAssembler) -
 				rhs: Register::GeneralPurpose(r),
 			}) => match op {
 				Op::Add => assembler.lea(GP[t], GP[l] + GP[r])?,
-				Op::Sub => todo!(),
-				Op::Abs => todo!(),
-				Op::Mul => todo!(),
-				Op::Div => todo!(),
-				Op::FAdd => todo!(),
-				Op::FSub => todo!(),
-				Op::FAbs => todo!(),
-				Op::FMul => todo!(),
-				Op::FDiv => todo!(),
-				Op::And => todo!(),
-				Op::Or => todo!(),
-				Op::Xor => todo!(),
-				Op::Not => todo!(),
-				Op::StoreMem => todo!(),
-				Op::LoadMem => todo!(),
-				Op::Move => todo!(),
+				Op::Sub => {
+					assembler.mov(GP[t], GP[l])?;
+					assembler.sub(GP[t], GP[r])?
+				}
+				Op::Mul => {
+					assembler.mov(GP[t], GP[l])?;
+					assembler.imul_2(GP[t], GP[r])?
+				}
+				Op::Div => {
+					assembler.mov(GP[t], GP[l])?;
+					// assembler.div(GP[t], GP[r])?
+					todo!()
+				}
+				Op::And => {
+					assembler.mov(GP[t], GP[l])?;
+					assembler.and(GP[t], GP[r])?
+				}
+				Op::Or => {
+					assembler.mov(GP[t], GP[l])?;
+					assembler.or(GP[t], GP[r])?
+				}
+				Op::Xor => {
+					assembler.mov(GP[t], GP[l])?;
+					assembler.xor(GP[t], GP[r])?
+				}
+				Op::StoreMem => {
+					assembler.mov(GP[t], GP[l])?;
+					// assembler.imul(GP[t], GP[r])?
+					todo!()
+				}
+				Op::LoadMem => {
+					assembler.mov(GP[t], GP[l])?;
+					// assembler.imul(GP[t], GP[r])?
+					todo!()
+				}
+				Op::Move => {
+					// Is this really ok?
+					assembler.mov(GP[t], GP[l] + GP[r])?
+				}
+
+				Op::Abs
+				| Op::Not
+				| Op::FAdd
+				| Op::FSub
+				| Op::FAbs
+				| Op::FMul
+				| Op::FDiv => {
+					bail!(Error::InvalidIR)
+				}
 			},
 			&Expression::UnOp(UnOp { target, op, lhs }) => match op {
 				Op::Add => todo!(),
@@ -125,12 +159,13 @@ fn assemble_block(Block { block, out }: &Block, assembler: &mut CodeAssembler) -
 				// Load arguments
 				// Copy rax to GP[target]
 				// Restore registers
+				todo!()
 			}
-			_ => unreachable!("Invalid IR"),
+			_ => bail!(Error::InvalidIR),
 		}
 	}
 	match out {
-		BlockEnd::Return => todo!(),
+		BlockEnd::Return => assembler.ret()?,
 		BlockEnd::One(_) => todo!(),
 		BlockEnd::Two(..) => todo!(),
 	}
