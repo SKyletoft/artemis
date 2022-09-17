90dbf244fcabba53ba9efd81b9b8cae4df4a2ecb
Samuel Kyletoft <samuel@kyletoft.se>
2022-09-17 23:35:56 +0200

    Callable object: Composed functions

--- c_working_files/callable_object.c
+++ c_working_files/callable_object.c
@@ -1,27 +1,32 @@
 #include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef int64_t (*FunctionPointer)(); // Variadic, not (void)
 
 typedef struct {
-	int64_t (*function)(); // Variadic, not (void)
+	FunctionPointer function;
 	int64_t count;
+	// Must always contain enough space for all the arguments of
+	// the underlying function, not how many are actually applied
 	int64_t *args;
 } Callable;
 
 typedef struct {
-	int64_t  count;
+	int64_t count;
 	Callable **functions;
-	int64_t  *first;
+	int64_t *first;
 } ComposedFunction;
 
 int64_t call_x64_callable_with_stack(
-	int64_t (*function)(),
+	FunctionPointer function,
 	int64_t count,
 	int64_t *args
 );
 
 int64_t call_x64_callable_no_stack(
-	int64_t (*function)(),
+	FunctionPointer function,
 	int64_t count,
 	int64_t *args
 );
@@ -34,55 +39,22 @@ int64_t call_x64_callable(Callable c) {
 	}
 }
 
-const char* long_str =
-	"%ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld\n";
-const char* short_str =
-	"%ld %ld %ld %ld\n";
+int64_t call_x64_callable_by_pointer(Callable *c) {
+	return call_x64_callable(*c);
+}
 
-int main() {
-	int64_t xs[15] = {
-		(int64_t) long_str,
-		1,
-		2,
-		3,
-		4,
-		5,
-		6,
-		7,
-		8,
-		9,
-		10,
-		11,
-		12,
-		13,
-		14
-	};
+int64_t call_composed_function(ComposedFunction f) {
+	int64_t last_ret = 0;
+	if (f.first) {
+		last_ret = *f.first;
+	}
+	for (ssize_t i = 0; i < f.count; i++) {
+		Callable c = *f.functions[i];
 
-	int64_t ys[5] = {
-		(int64_t) short_str,
-		1,
-		2,
-		3,
-		4,
-	};
+		c.args[c.count] = last_ret;
+		c.count++;
 
-	Callable long_printf_callable = (Callable){
-		.function = &printf,
-		.count    = 15,
-		.args     = xs,
-	};
-
-	Callable short_printf_callable = (Callable){
-		.function = &printf,
-		.count    = 5,
-		.args     = ys,
-	};
-
-	int64_t correct1 = printf(short_str, 1, 2, 3, 4);
-	int64_t correct2 = printf(long_str, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
-
-	int64_t ret1 = call_x64_callable(short_printf_callable);
-	int64_t ret2 = call_x64_callable(long_printf_callable);
-
-	printf("%ld (%ld) %ld (%ld)\n", ret1, correct1, ret2, correct2);
+		last_ret = call_x64_callable(c);
+	}
+	return last_ret;
 }
