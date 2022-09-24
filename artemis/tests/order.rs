b688a747b1ec10b30bd3894a6b7c2ce344876d52
Samuel Kyletoft <samuel@kyletoft.se>
2022-09-25 01:25:52 +0200

    Remove outdated if else if test

--- artemis/tests/order.rs
+++ artemis/tests/order.rs
@@ -278,25 +278,6 @@ fn op_assign() {
 }
 
 #[test]
-fn else_if() {
-	let a = "if () {()} else if () {()} else {()}";
-	let b = "if () {()} else { if () {()} else {()} }";
-	let res_a = GeneratedParser::parse(Rule::expr, a)
-		.unwrap()
-		.map(AST::try_from)
-		.collect::<Result<Vec<_>>>()
-		.unwrap();
-	let res_b = GeneratedParser::parse(Rule::expr, b)
-		.unwrap()
-		.map(AST::try_from)
-		.collect::<Result<Vec<_>>>()
-		.unwrap();
-	assert_eq!(res_a.len(), 1);
-	assert_eq!(res_b.len(), 1);
-	assert_eq!(res_a, res_b);
-}
-
-#[test]
 fn declaration_with_variable_init() {
 	let s = "{
 		x : ℤ = 1
