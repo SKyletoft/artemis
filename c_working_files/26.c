92878610ca512d529b6837c6003aa595c6ad3282
Samuel Kyletoft <samuel@kyletoft.se>
2022-10-01 03:08:03 +0200

    Update C version of example 26

--- c_working_files/26.c
+++ c_working_files/26.c
@@ -3,11 +3,16 @@
 typedef unsigned long N;
 typedef signed long Z;
 
+Z print_n(N a) {
+	return printf("%d\n", a);
+}
+
 N g (N b) { return b * 4; }
 
 N h (N a, N b, N c, N d) {return a + b * c - d;}
 
 N i (N a, N b, N c, N d, N e, N f, N g, N h) {return a + b * c - d;}
+
 N l (N a, N b, N c, N d, N e, N f, N g, N h, N i) {return a + b * c - d;}
 
 N j (N x) {return x;}
@@ -17,17 +22,25 @@ N k (N x) {
 }
 
 N f(N a) {
-	return	0
-		+ g(a)
-		+ h(a+ 2, 1, 2, 3)
-		+ i(1, 2, 3, 4, 5, 6, 7, 8)
-		+ j(8)
-		+ k(2)
-		+ l(1, 2, 3, 4, 5, 6, 7, 8, 9);
+	N a2 = g(a);
+	N a3 = h(a+ 2, 1, 2, 3);
+	N a4 = i(1, 2, 3, 4, 5, 6, 7, 8);
+	N a5 = j(8);
+	N a6 = k(2);
+	N a7 = l(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+	print_n(a2);
+	print_n(a3);
+	print_n(a4);
+	print_n(a5);
+	print_n(a6);
+	print_n(a7);
+
+	return a2 + a3 + a4 + a5 + a6 + a7;
 }
 
 int main() {
-	N f1 = f(0);
-	N f0 = f(1);
-	printf("%ld %ld\n", f0, f1);
+	N f0 = f(0);
+	N f1 = f(1);
+	printf("%ld, %ld\n", f0, f1);
 }
