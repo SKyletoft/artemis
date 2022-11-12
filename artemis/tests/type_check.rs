b211060597daf265a77109b1b41b03b432a5754f
Samuel Kyletoft <samuel@kyletoft.se>
2022-11-12 01:42:24 +0100

    Update format of type tests

--- artemis/tests/type_check.rs
+++ artemis/tests/type_check.rs
@@ -1,11 +1,5 @@
 use anyhow::Result;
-use artemis::{
-	ordered::{
-		self, Assignment, Declaration, Expr, Function, Literal, RawType, Term,
-		TopLevelConstruct, Type,
-	},
-	type_check, GeneratedParser, Rule,
-};
+use artemis::{ordered, preprocess, types, GeneratedParser, Rule, error::Error};
 use once_cell::sync::Lazy;
 use pest::Parser;
 use simple_logger::SimpleLogger;
@@ -14,86 +8,83 @@ use smallvec::SmallVec;
 #[allow(unused_variables, dead_code)]
 static LOGGER: Lazy<()> = Lazy::new(|| SimpleLogger::new().init().expect("Logger init failure"));
 
-#[test]
-fn assignment_to_same() -> Result<()> {
-	let s = "λf() = {
-		x : ℤ = 1
-		y : ℤ = x;
-		()
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	type_check::check_program(&mut ordered)?;
-	Ok(())
+fn compile_and_typecheck(code: String) -> Result<()> {
+	log::trace!("{}", &code);
+
+	let source = preprocess::remove_comments(&[code]);
+	let ast = GeneratedParser::parse(Rule::top, &source)?;
+	let ordered = ordered::order(ast)?;
+	types::check_and_infer(ordered).map(|_| ())
 }
 
 #[test]
-fn literals_as_either_type() -> Result<()> {
-	let s = "λf() = {
+fn assignment_to_same() {
+	let s = "λf () = (
+		x : ℤ = 1
+		y : ℤ = x
+		()
+	)".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
+}
+
+#[test]
+fn literals_as_either_type() {
+	let s = "λf () = {
 		x : ℤ = 1
 		y : ℕ = 1;
 		()
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	type_check::check_program(&mut ordered)?;
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
 
 #[test]
-fn reject_integer_conversion() -> Result<()> {
-	let s = "λf() = {
+fn reject_integer_conversion() {
+	let s = "λf () = (
 		x : ℤ = 1
 		y : ℕ = x;
 		()
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	let res = type_check::check_program(&mut ordered);
-
+	)".into();
+	let res = compile_and_typecheck(s);
 	assert!(res.is_err());
-	Ok(())
+	assert!(matches!(res.unwrap_err().downcast_ref::<Error>(), Some(Error::MismatchedTypes(_))));
 }
 
 #[test]
-fn reject_non_bool_condition() -> Result<()> {
-	let s = "λf() = {
+fn reject_non_bool_condition() {
+	let s = "λf () = (
 		if 1 {()} else {()}
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	let res = type_check::check_program(&mut ordered);
-
+	)".into();
+	let res = compile_and_typecheck(s);
 	assert!(res.is_err());
-	Ok(())
+	assert!(matches!(res.unwrap_err().downcast_ref::<Error>(), Some(Error::MismatchedTypes(_))));
 }
 
 #[test]
-fn reject_same_scope_same_type_shadowing() -> Result<()> {
+fn reject_same_scope_same_type_shadowing() {
 	let s = "λf() = {
 		x := 1
 		x := 1
 		()
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	let res = type_check::check_program(&mut ordered);
-
-	assert!(res.is_err());
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
 
 #[test]
-fn reject_same_scope_different_type_shadowing() -> Result<()> {
+fn reject_same_scope_different_type_shadowing() {
 	let s = "λf() = {
 		x := 1
 		x := true
 		()
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	let res = type_check::check_program(&mut ordered);
-
-	assert!(res.is_err());
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
 
 #[test]
-fn accept_inner_scope_shadowing() -> Result<()> {
+fn accept_inner_scope_shadowing() {
 	let s = "λf() = {
 		x := 1
 		y := {
@@ -101,79 +92,64 @@ fn accept_inner_scope_shadowing() -> Result<()> {
 			x
 		}
 		()
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	type_check::check_program(&mut ordered)?;
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
 
 #[test]
-fn reject_use_of_undeclared_variable() -> Result<()> {
+fn reject_use_of_undeclared_variable() {
 	let s = "λf() -> ℕ = {
 		x
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	let res = type_check::check_program(&mut ordered);
-
-	assert!(res.is_err());
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
 
 #[test]
-fn reject_use_of_undeclared_function() -> Result<()> {
+fn reject_use_of_undeclared_function() {
 	let s = "λf() -> ℕ = {
 		g()
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	let res = type_check::check_program(&mut ordered);
-
-	assert!(res.is_err());
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
 
 #[test]
-fn reject_use_of_variable_as_function() -> Result<()> {
+fn reject_use_of_variable_as_function() {
 	let s = "λf() = {
 		x := 1
 		y := x()
 		()
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	let res = type_check::check_program(&mut ordered);
-
-	assert!(res.is_err());
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
 
 #[test]
-fn reject_assignment_to_const() -> Result<()> {
+fn reject_assignment_to_const() {
 	let s = "λf() = {
 		x : ℤ = 1
 		x = 2
 		()
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	let res = type_check::check_program(&mut ordered);
-
-	assert!(res.is_err());
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
 
 #[test]
-fn accept_assignment_to_mut() -> Result<()> {
+fn accept_assignment_to_mut() {
 	let s = "λf() = {
 		x : mut ℤ = 1
 		x = 2
 		()
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	type_check::check_program(&mut ordered)?;
-
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
 
 #[test]
-fn accept_if_returns_same() -> Result<()> {
+fn accept_if_returns_same() {
 	let s = "λf() = {
 		x : ℝ = if true {
 			1.0
@@ -181,14 +157,12 @@ fn accept_if_returns_same() -> Result<()> {
 			2.0
 		}
 		()
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	type_check::check_program(&mut ordered)?;
-
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
 #[test]
-fn reject_if_returns_different() -> Result<()> {
+fn reject_if_returns_different() {
 	let s = "λf() = {
 		x := if true {
 			true
@@ -196,16 +170,13 @@ fn reject_if_returns_different() -> Result<()> {
 			1.0
 		}
 		()
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	let res = type_check::check_program(&mut ordered);
-
-	assert!(res.is_err());
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
 
 #[test]
-fn accept_block_return_of_inner_variable() -> Result<()> {
+fn accept_block_return_of_inner_variable() {
 	let s = "λf (x: ℤ) → ℤ = {
 		a := {
 			a : mut ℤ = 1
@@ -213,78 +184,29 @@ fn accept_block_return_of_inner_variable() -> Result<()> {
 			a
 		}
 		a + 2
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	type_check::check_program(&mut ordered)?;
-
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
 
 #[test]
-fn infer_types() -> Result<()> {
+fn infer_types() {
 	let s = "λf () = {
 		x := 1
 		y := true
 		z := -2
 		w := 4.0
 		()
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	type_check::check_program(&mut ordered)?;
-	let expected = vec![TopLevelConstruct::Function(Function {
-		name: "f".into(),
-		arguments: SmallVec::new(),
-		return_type: RawType::Unit,
-		expr: Expr::Term(Term::Block(vec![
-			Expr::Declaration(Declaration {
-				name: "x".into(),
-				value: Box::new(Expr::Term(Term::Literal(Literal::Integer(1)))),
-				type_name: Type {
-					mutable: false,
-					raw: RawType::Integer,
-				},
-			}),
-			Expr::Declaration(Declaration {
-				name: "y".into(),
-				value: Box::new(Expr::Term(Term::Literal(Literal::Boolean(true)))),
-				type_name: Type {
-					mutable: false,
-					raw: RawType::Boolean,
-				},
-			}),
-			Expr::Declaration(Declaration {
-				name: "z".into(),
-				value: Box::new(Expr::Term(Term::Literal(Literal::Integer(
-					-2i64 as u64,
-				)))),
-				type_name: Type {
-					mutable: false,
-					raw: RawType::Integer,
-				},
-			}),
-			Expr::Declaration(Declaration {
-				name: "w".into(),
-				value: Box::new(Expr::Term(Term::Literal(Literal::Float(4.0)))),
-				type_name: Type {
-					mutable: false,
-					raw: RawType::Real,
-				},
-			}),
-			Expr::Term(Term::Literal(Literal::Unit)),
-		])),
-	})];
-	assert_eq!(ordered, expected);
-
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
 
 #[test]
-fn accept_ending_on_declaration() -> Result<()> {
+fn accept_ending_on_declaration() {
 	let s = "λf () → ℤ = {
 		x := 5
-	}";
-	let mut ordered = ordered::order(GeneratedParser::parse(Rule::top, s.trim())?)?;
-	type_check::check_program(&mut ordered)?;
-
-	Ok(())
+	}".into();
+	let res = compile_and_typecheck(s);
+	assert!(matches!(res, Ok(())))
 }
