fdee531dccbdf90bddb0305acdd84dc2a47d6b84
Samuel Kyletoft <samuel@kyletoft.se>
2023-01-21 19:14:54 +0100

    Remove main from runtime

--- c_working_files/runtime.c
+++ c_working_files/runtime.c
@@ -36,11 +36,11 @@ int64_t print_b(int64_t a) {
 	return a;
 }
 
-int main() {
-	int64_t a = f(0);
-	int64_t b = f(1);
+// int main() {
+// int64_t a = f(0);
+// int64_t b = f(1);
 
-	printf("%ld, %ld\n", a, b);
+// printf("%ld, %ld\n", a, b);
 
-	return 0;
-}
+// return 0;
+// }
